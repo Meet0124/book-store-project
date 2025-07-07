2113e2261b8a343d5da1eda391a4c8ac3a7c8d22
Meet Adlakha <meet.adlakha@gmail.com>
2025-07-07 22:47:57 +0530

    working on home page

--- frontend/bookstore/src/main.jsx
+++ frontend/bookstore/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from "react-router";
+
+import router from './routers/router.jsx'
+import { BrowserRouter, RouterProvider } from "react-router";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
---
+++ frontend/bookstore/tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': "#FFCE1A",
+        'secondary': "#0D0842",
+        'blackBG': "#f3f3f3",
+        'Favourite': '#FF5841'
+      },
+    },
+  },
+  plugins: [],
+};
